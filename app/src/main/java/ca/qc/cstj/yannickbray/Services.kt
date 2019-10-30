a30d377eb2d73387fa9df036b0a2b629633f77d0
Mooo333 <1647147@cstj.qc.ca>
2019-10-30 16:26:43 -0400

    Initial commit

---
+++ app/src/main/java/ca/qc/cstj/yannickbray/Services.kt
@@ -0,0 +1,16 @@
+package ca.qc.cstj.yannickbray
+
+class Services {
+    companion object {
+        // URL A
+        // Toutes succursales
+
+        // Toutes catégorie
+        val CATEGORIES_API_URL = "localhost:4500/livres/categories"
+
+        // URL B
+
+        // URL C
+
+    }
+}
